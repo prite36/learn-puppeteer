3e8313ffb21a67d6d49ff5f41cba64a942ce0a9f
prite36 <spritelemon36@hotmail.com>
2018-12-03 02:36:29 +0700

    test heutron.com

--- server.js
+++ server.js
@@ -1,27 +1,38 @@
 // import puppeteer เข้ามา
 const puppeteer = require('puppeteer');
 
-(async () => {
-  // เปิด Browser ขึ้นมา
+(async() => {
   const browser = await puppeteer.launch({headless: false});
-  // เปิด page ขึ้นมา
   const page = await browser.newPage();
-  // เข้าเว็บซักเว็บนึง
-  await page.goto('https://www.kapook.com');
-  
-  // ถ้าจะกรอกอะไรก่อนหรือ กดอะไรก่อนก็ทำตรงนี้
-  // page.focus('#search')
-  // page.type('Hello')
-  // page.click('#search_btn')
-  
-  const dimensions = await page.evaluate(() => {
-    // code ส่วนนี้จะถูกรันบนหน้าเพจ เหมือนเราไปพิมพ์ใน console 
-    return {
-      width: document.documentElement.clientWidth,
-      height: document.documentElement.clientHeight,
-      deviceScaleFactor: window.devicePixelRatio
-    };
-  });
-  // ปิด Browser 
-//   await browser.close();
+  // await this.page.setViewport({width: 1920, height: 1080});
+  await page.goto('https://heutron.com/yc', {waitUntil: 'networkidle2'});
+  await Promise.all([
+    page.click('.administered_litigation .racer_forgotten:nth-child(3) .content_information>div'),
+    page.waitForNavigation({waitUntil: 'networkidle2'}),
+  ]);
+  page.waitFor(10000)
+  // await Promise.all([
+  //   page.click('.administered_litigation .racer_forgotten:nth-child(4) .content_information>div'),
+  //   page.waitForNavigation({waitUntil: 'networkidle2'}),
+  // ]);
+  // // Wait for suggest overlay to appear and click "show all results".
+  // const allResultsSelector = 'const allResultsSelector = 'div.odor_excite .kite_molecule .walnut_expiry>div';';
+  // await page.waitForSelector(allResultsSelector);
+  // await page.click(allResultsSelector);
+
+  // // Wait for the results page to load and display the results.
+  // const resultsSelector = '.gsc-results .gsc-thumbnail-inside a.gs-title';
+  // await page.waitForSelector(resultsSelector);
+
+  // // Extract the results from the page.
+  // const links = await page.evaluate(resultsSelector => {
+  //   const anchors = Array.from(document.querySelectorAll(resultsSelector));
+  //   return anchors.map(anchor => {
+  //     const title = anchor.textContent.split('|')[0].trim();
+  //     return `${title} - ${anchor.href}`;
+  //   });
+  // }, resultsSelector);
+  // console.log(links.join('\n'));
+
+  await browser.close();
 })();
